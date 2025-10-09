a290e18bc74bc7b91a764529f39f867a49396ac4
Petrus79 <petrus_1@gmx.de>
2025-10-09 10:23:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ if [ ! -f /etc/pyHPSU/pyhpsu.conf ]; then
     cp /opt/pyHPSU/etc/pyHPSU/pyhpsu.conf /etc/pyHPSU/pyhpsu.conf
 fi
 
-pyHPSU.py --mqtt_daemon --log_level DEBUG -a -o MQTT
+pyHPSU.py --mqtt_daemon --log_level DEBUG -a -o MQTTDAEMON
